5eca75d08dfe70dca57d5f95d80a140533fad1e8
Abhishek <80678205+abhishekcs20@users.noreply.github.com>
2022-07-11 22:29:55 +0530

    Create js.js

---
+++ js.js
@@ -0,0 +1,10 @@
+var stuname= document.getElementById("student-name");
+var clgroll= document.getElementById("clgRoll");
+var address= document.getElementById("address");
+var date= document.getElementById("date");
+window.onload=()=>{
+    stuname.innerHTML="Anamika Varshney";
+    clgroll.innerText="201500085";
+    address.innerText="GLA University,Mathura";
+    date.innerText="11-07-2022";
+}
